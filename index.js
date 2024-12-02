cbae232a0450a764a40cf5b162902c20fccb63d4
Damaris Ngari <wanjirungari2@gmail.com>
2024-12-03 00:18:08 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+ customerName = customerName.toUpperCase();
+
+}
+
+//3. setbestcustomer
+var bestCustomer;
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+
+//overwrite bestcustomer
+
+let BestCustomer;
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer () {
+ bestCustomer = 'maybe bob';
+}
+
+//changeleast
+const LeastFavoriteCustomer = 'bob';
+function changeLeastFavoriteCustomer () {
+    LeastFavoriteCustomer = 'joy';
+}
